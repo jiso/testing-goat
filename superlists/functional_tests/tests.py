df8bfd9b96b52b173827b82f7da5949abe8600dd
Jia-Shing So <jiashing@gmail.com>
2015-08-06 16:41:48 -0700

    new URL, view, and template to display lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -84,7 +84,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('make a fl', page_text)
         
         # Francis starts a new list by entering a new item.  He is less interesting than Edith ...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.driver.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         
