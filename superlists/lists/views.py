b2552443b506b63a13a59f77e5fb85b9d4696363
Jia-Shing So <jiashing@gmail.com>
2015-08-18 17:17:45 -0700

    Refactor list view to handle new item POSTs

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,8 +6,18 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'lists/home.html')
 
+# def view_list(request, list_id):
+#     list_ = List.objects.get(id=list_id)
+#     if request.method == 'POST':
+#         Item.objects.create(text = request.POST['item_text'], list=list_)
+#         return redirect('/lists/%d/' % (list_.id,))
+#     return render(request, 'lists/list.html', {'list': list_})
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'lists/list.html', {'list': list_})
 
 def new_list(request):
@@ -20,9 +30,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'lists/home.html', {"error": error})
-    return redirect('/lists/%d/' % list_.id)
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % list_.id)
